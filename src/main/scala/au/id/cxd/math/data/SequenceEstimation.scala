1450d55344f2191105a1131aa43d18e41e3b3465
Chris Davey <cd@cpd.id.au>
2015-01-15 22:25:14 +1000

    translating estimation module to library

--- src/main/scala/au/id/cxd/math/data/SequenceEstimation.scala
+++ src/main/scala/au/id/cxd/math/data/SequenceEstimation.scala
@@ -1,5 +1,9 @@
 package au.id.cxd.math.data
 
+import breeze.linalg._
+import breeze.math._
+import breeze.numerics._
+
 /**
  * Created by cd on 12/01/15.
  */
@@ -9,9 +13,9 @@ class SequenceEstimation {
    * extract states from the supplied data
    * @param dataSet
    */
-  def extractStateSequence (dataSet:List[List[String]]):List[String] = {
+  def extractStateSequence(dataSet: List[List[String]]): List[String] = {
     dataSet.foldLeft(List[String]()) {
-      (accum, data) =>{
+      (accum, data) => {
         val state = data.reverse.head
         state :: accum
       }
@@ -19,6 +23,22 @@ class SequenceEstimation {
       .sorted
   }
 
+  /**
+   * extract states from the supplied data
+   * @param dataSet
+   */
+  def extractStateSeq(dataSet: List[List[String]])(state: String): List[List[String]] = {
+    dataSet.foldLeft(List[List[String]]()) {
+      (accum, data) => {
+        val state2 = data.reverse.head
+        state2.equalsIgnoreCase(state) match {
+          case true => data :: accum
+          case _ => accum
+        }
+      }
+    }.distinct
+      .sorted
+  }
 
   /**
    * extract all sequences whose last "term" (the item before the state) is equal to the end term.
@@ -26,7 +46,7 @@ class SequenceEstimation {
    * @param endTerm
    * @return
    */
-  def extractEndTermSeq (dataSet:List[List[String]]) (endTerm:String): List[List[String]] = {
+  def extractEndTermSeq(dataSet: List[List[String]])(endTerm: String): List[List[String]] = {
     dataSet.foldLeft(List[List[String]]()) {
       (accum, data) => {
         val state = data.reverse.head
@@ -46,10 +66,10 @@ class SequenceEstimation {
    * @param states
    * @return
    */
-  def countStateFreq (dataSet:List[List[String]]) (states:List[String]) : List[(Double, String)] = {
+  def countStateFreq(dataSet: List[List[String]])(states: List[String]): List[(Double, String)] = {
     states.map {
       state => {
-        dataSet.foldLeft (1.0, state) {
+        dataSet.foldLeft(1.0, state) {
           (pair, data) => {
             val (n, state2) = pair
             val other = data.reverse.head
@@ -69,9 +89,9 @@ class SequenceEstimation {
    * @param states
    * @return
    */
-  def statePriors (dataSet:List[List[String]]) (states:List[String]) : List[(Double, String)] = {
+  def statePriors(dataSet: List[List[String]])(states: List[String]): List[(Double, String)] = {
     val frequencies = countStateFreq(dataSet)(states)
-    val sum = frequencies.foldLeft (0.0) {
+    val sum = frequencies.foldLeft(0.0) {
       (total, pair) => {
         val (n, state) = pair
         n + total
@@ -91,7 +111,7 @@ class SequenceEstimation {
    * @param b
    * @return
    */
-  def matches (a:String) (b:String) = {
+  def matches(a: String)(b: String) = {
     a.equalsIgnoreCase(b)
   }
 
@@ -102,14 +122,14 @@ class SequenceEstimation {
    * @param listB
    * @return
    */
-  def countDelta (listA:List[String]) (listB:List[String]) : Double = {
+  def countDelta(listA: List[String])(listB: List[String]): Double = {
     val len = listA.length
     val test = listB.take(len)
     val pairs = listA.zip(test)
     pairs.foldLeft(0.0) {
       (n, pair) => {
         val (itemA, itemB) = pair
-        matches (itemA) (itemB) match {
+        matches(itemA)(itemB) match {
           case true => n
           case false => n + 1.0
         }
@@ -125,7 +145,7 @@ class SequenceEstimation {
    * @param listB
    * @return
    */
-  def countDeltaState (listA:List[String]) (listB:List[String]) : Double = {
+  def countDeltaState(listA: List[String])(listB: List[String]): Double = {
     val arr = listA.toIndexedSeq
     val test = listB.indices.map {
       i => {
@@ -134,10 +154,10 @@ class SequenceEstimation {
       }
     }
     val pairs = test.zip(listB)
-    pairs.foldLeft (0.0) {
+    pairs.foldLeft(0.0) {
       (n, pair) => {
         val (itemA, itemB) = pair
-        matches (itemA) (itemB) match {
+        matches(itemA)(itemB) match {
           case true => n + 1.0
           case _ => n
         }
@@ -154,8 +174,298 @@ class SequenceEstimation {
    * @param stateB
    * @return
    */
-  def countTransitionFreq (dataSet:List[List[String]]) (stateA:String) (stateB:String) : Double = {
-    0.0
+  def countTransitionFreq(dataSet: List[List[String]])(stateA: String)(stateB: String): Double = {
+    // find sequences in the data set that end in the supplied state
+    def findSeq(state: String) = {
+      dataSet.foldLeft(List[List[String]]()) {
+        (accum, data) => {
+          val otherState = data.reverse.head
+          otherState.equalsIgnoreCase(state) match {
+            case true => data :: accum
+            case _ => accum
+          }
+        }
+      }
+    }
+
+    val startSeqs = findSeq(stateA)
+    val endSeqs = findSeq(stateB)
+
+    val freqs = startSeqs.map {
+      start => {
+        // find others 1 longer than start
+        val otherSeqs = endSeqs.filter {
+          end => end.length == (start.length + 1)
+        }
+        otherSeqs.foldLeft(1.0) {
+          (n, otherSeq) => {
+            val delta = countDeltaState(start)(otherSeq)
+            delta > 2 match {
+              case false => n + 1.0
+              case _ => n
+            }
+          }
+        }
+      }
+    }
+    freqs.length == 0 match {
+      case true => 1.0
+      case _ => freqs.sum
+    }
+  }
+
+  /**
+   * count transitions between states
+   * from stateA (i) to stateB (j)
+   * @param dataSet
+   * @param states
+   * @return
+   */
+  def countTransitions(dataSet: List[List[String]])(states: List[String]): DenseMatrix[Double] = {
+    val stateArr = states.toIndexedSeq
+    DenseMatrix.tabulate(states.length, states.length) {
+      (i, j) => {
+        val stateA = stateArr.apply(i)
+        val stateB = stateArr.apply(j)
+        countTransitionFreq(dataSet)(stateA)(stateB)
+      }
+    }
+  }
+
+  /**
+   * determine the probability for the state transitions
+
+    from stateA (i) to stateB (j)
+
+    a_ij = P(x_j | x_i)
+
+    Note each row will sum to 1.
+   * @param dataSet
+   * @param states
+   * @return
+   */
+  def stateTransitions(dataSet: List[List[String]])(states: List[String]): DenseMatrix[Double] = {
+    val mat = countTransitions(dataSet)(states)
+    // normalise each row of the matrix to 1.0
+    normalize(mat(*, ::), 1.0)
+  }
+
+  /**
+   * count the occurance of the term in the sequence
+    this will always add 1.0 to every term count so there are no 0 values.
+   * @param items
+   * @param term
+   * @return
+   */
+  def countTermInSeq(items: List[String])(term: String): Double = {
+    items.foldLeft(1.0) {
+      (n, item) => {
+        item.equalsIgnoreCase(term) match {
+          case true => n + 1.0
+          case _ => n
+        }
+      }
+    }
+  }
+
+  /**
+   * determine whether the end term matches the supplied term from the sequence
+
+    The sequence is assumed to contain
+
+    [term1; term2; ...; termN; state]
+
+    This will reverse the term list and remove the state
+    it will then determine whether termN matches the supplied term.
+
+   * @param items
+   * @param term
+   * @return
+   */
+  def countTermInEndOfSeq(items: List[String])(term: String): Double = {
+    val rev = items.reverse
+    val endTerm = rev.tail.head
+    endTerm.equalsIgnoreCase(term) match {
+      case true => 2.0
+      case _ => 1.0
+    }
+  }
+
+  /**
+   * count the frequency of the term in the sequence
+    return a tuple (double * double)
+    where _1 = term frequency for supplied state
+    _2 = total term frequency in subseq
+
+    This method will always start with 1.0 adding 1.0 to every term count
+    so that there is no 0.0 values
+   * @param subseq
+   * @param term
+   * @param state
+   * @return
+   */
+  def countTermInState(subseq: List[List[String]])(term: String)(state: String): (Double, Double) = {
+    val stateSeq = extractStateSeq(subseq)(state)
+    val stateCount =
+      stateSeq.foldLeft(1.0) {
+        (n, items) => {
+          n + countTermInSeq(items)(term)
+        }
+      }
+    val totalCount =
+      subseq.foldLeft(1.0) {
+        (n, items) => {
+          n + countTermInSeq(items)(term)
+        }
+      }
+    (stateCount, totalCount)
+  }
+
+  /**
+   * this will count the total number of times a term appears at the end of a sequence
+    for the supplied state label
+
+    It will then count the total number of occurances of the term in all sequences
+
+    and will return the tuple
+
+    (endOfSeqCount, totalCount)
+   * @param subseq
+   * @param term
+   * @param state
+   * @return
+   */
+  def countTermAtEndOfState(subseq: List[List[String]])(term: String)(state: String): (Double, Double) = {
+    val stateSeq = extractStateSeq(subseq)(state)
+    val stateCount =
+      stateSeq.foldLeft(1.0) {
+        (n, items) => {
+          n + countTermInEndOfSeq(items)(term)
+        }
+      }
+    val totalCount =
+      subseq.foldLeft(1.0) {
+        (n, items) => {
+          n + countTermInSeq(items)(term)
+        }
+      }
+    (stateCount, totalCount)
+  }
+
+  /**
+   * determine the prior evidence matrix P(e_i | x_j)
+    for state transitions
+
+    each row will normalise to 1.
+   * @param data
+   * @param terms
+   * @param states
+   * @return
+   */
+  def priorEvidence(data: List[List[String]])(terms: List[String])(states: List[String]): DenseMatrix[Double] = {
+    val m = terms.length
+    val n = states.length
+    val termSeq = terms.toIndexedSeq
+    val stateSeq = states.toIndexedSeq
+    val mat = DenseMatrix.tabulate(m, n) {
+      (i, j) => {
+        val (freq, totalFreq) = countTermInState(data)(termSeq.apply(i))(stateSeq.apply(j))
+        freq / totalFreq
+      }
+    }
+    // normalise each row of the matrix to 1.0
+    normalize(mat(*, ::), 1.0)
+  }
+
+  /**
+   * calculate the prior evidences for all data sets
+   * @param data
+   * @param terms
+   * @param states
+   * @return
+   */
+  def priorEvidences(data: List[List[String]])(terms: List[String])(states: List[String]) = {
+    terms.map {
+      (term) => {
+        val data = extractEndTermSeq(data)(term)
+        priorEvidence(data)(terms)(states)
+      }
+    }
+  }
+
+  /**
+   * calculate the prior evidences for all data sets
+   * assume each sample is independently and identically distributed
+   * return the average prior evidences
+   * @param data
+   * @param terms
+   * @param states
+   * @return
+   */
+  def avgPriorEvidences(data: List[List[String]])(terms: List[String])(states: List[String]) = {
+    val m = states.length
+    val n = terms.length
+
+    def innerPriorEvidence(data: List[List[String]])(terms: List[String])(states: List[String]):(DenseVector[Double], DenseMatrix[Double]) = {
+      val stateArr = states.toIndexedSeq
+      val termArr = terms.toIndexedSeq
+      val result =
+        (0 to (n - 1)).foldLeft(List[((Int, Int), (Double, Double))]()) {
+          (accum, i) => {
+            (0 to (m - 1)).foldLeft(accum) {
+              (arr, j) => {
+                val (freq, totalFreq) = countTermAtEndOfState(data)(termArr.apply(j))(stateArr.apply(i))
+
+                ((i, j), (freq, totalFreq)) :: arr
+              }
+            }
+          }
+        }
+      val totalVec = DenseVector.tabulate(n) {
+        (i) => {
+          result.foldLeft (0.0) {
+            (n1, pair) => {
+              val ((a, b), (f, t)) = pair
+              (i == a) match {
+                case true => {
+                  n1 + t
+                }
+                case _ => n1
+              }
+            }
+          }
+        }
+      }
+
+      val mat = DenseMatrix.tabulate(n, m) {
+        (i, j) => {
+          val total = totalVec.apply(i)
+          val freq =
+            result.foldLeft (0.0) {
+              (n1, pair) => {
+                val ((a, b), (f, t)) = pair
+                ((i == a) && (j == b)) match {
+                  case true => f
+                  case _ => n1
+                }
+              }
+            }
+          freq
+        }
+      }
+      (totalVec, mat)
+    }
+
+    val pairs = innerPriorEvidence(data) (terms) (states)
+    val V = pairs._1
+    val B = pairs._2
+    DenseMatrix.tabulate(n, m) {
+      (i, j) => {
+        val t = V.apply(i)
+        val f = B.apply(i, j)
+        f / t
+      }
+    }
   }
 
 }
