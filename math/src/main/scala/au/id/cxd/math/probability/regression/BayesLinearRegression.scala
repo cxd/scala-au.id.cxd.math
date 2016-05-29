686591ce579a577e28981f1779af59e771be98f6
Chris Davey <cd@cpd.id.au>
2016-05-29 20:34:54 +1000

    begin work on update function

--- math/src/main/scala/au/id/cxd/math/probability/regression/BayesLinearRegression.scala
+++ math/src/main/scala/au/id/cxd/math/probability/regression/BayesLinearRegression.scala
@@ -1,5 +1,10 @@
 package au.id.cxd.math.probability.regression
 
+import au.id.cxd.math.function.PseudoInverse
+import au.id.cxd.math.probability.analysis.CriticalValue
+import au.id.cxd.math.probability.continuous.Normal
+import breeze.linalg.{DenseMatrix, DenseVector}
+
 /**
   * ##import MathJax
   *
@@ -145,34 +150,34 @@ package au.id.cxd.math.probability.regression
   * The next step is to marginalise over $\sigma$ to obtain the predictive density.
   *
   *     $$
-p(\tilde{Y}|\tilde{X},Y,X) \propto \int p(\tilde{Y}|\sigma,\tilde{X},Y,X) \times p(\sigma|Y,X) d\sigma
-$$
-
-$$
-p(\tilde{Y}|\tilde{X},Y,X) \propto \int \sigma&#94;{-(n - k + 1)} \exp{\left[ \frac{\nu s&#94;2}{2\sigma&#94;2} \right]} \times
-$$
-
-$$
-(\sigma&#94;2)&#94;{-n/2}\left|I + \tilde{X}V\tilde{X}'\right|&#94;{-n/2}\exp{\left[-\frac{1}{2\sigma&#94;2}(\tilde{X} - \tilde{X}\hat{\beta})'\left[I + \tilde{X}V\tilde{X}' \right]&#94;{-1}(\tilde{X} - \tilde{X}\hat{\beta}) \right]} d\sigma
-$$
-
-$$
-\propto \int \sigma&#94;{-(n - k + 1)/2}\left|I + \tilde{X}V\tilde{X}'\right|&#94;{-n/2} \exp{\left[ -\frac{1}{2\sigma&#94;2}\left( \nu s&#94;2  + (\tilde{X} - \tilde{X}\hat{\beta})'\left[I + \tilde{X}V\tilde{X}' \right]&#94;{-1}(\tilde{X} - \tilde{X}\hat{\beta}) \right) \right]} d\sigma
-$$
-$$
-\propto \left|I + \tilde{X}V\tilde{X}'\right|&#94;{-n/2} \int \sigma&#94;{-(\nu + 1)/2} \exp{\left[-\frac{1}{2\sigma&#94;2}A \right]} d\sigma&#94;2
-$$
-Where $A = \left( \nu s&#94;2  + (\tilde{X} - \tilde{X}\hat{\beta})'\left[I + \tilde{X}V\tilde{X}' \right]&#94;{-1}(\tilde{X} - \tilde{X}\hat{\beta}) \right)$ and we substitute $z = \frac{A}{2\sigma&#94;2}$ as described in "Quadratic form statistics" on wikipedia (see below) to produce
-$$
-p(\tilde{Y}|Y,\tilde{X},X) \propto A&#94;{-\frac{\nu + 1}{2}}\int z&#94;{(\nu - 1)/2}\exp(-z)dz
-$$
-which evaluates to
-$$
-\left[ 1 + \frac{(\tilde{X} - \tilde{X}\hat{\beta})'\left[I + \tilde{X}V\tilde{X}' \right]&#94;{-1}(\tilde{X} - \tilde{X}\hat{\beta})}{\nu s&#94;2} \right]&#94;{-\frac{1}{2}(\nu + 1)}
-$$
-which is a multivariate t-distribution centered at $\tilde{X}\hat{\beta}$ and scaled by $[I+\tilde{X}V\tilde{X}'] s&#94;2$ with $\nu = n - k$ degrees of freedom.
-
-  * The posterior predictive density can be used to draw samples in performing simulation of the distribution of $\tilde{Y}$.
+  * p(\tilde{Y}|\tilde{X},Y,X) \propto \int p(\tilde{Y}|\sigma,\tilde{X},Y,X) \times p(\sigma|Y,X) d\sigma
+  * $$
+  **
+  *$$
+  *p(\tilde{Y}|\tilde{X},Y,X) \propto \int \sigma&#94;{-(n - k + 1)} \exp{\left[ \frac{\nu s&#94;2}{2\sigma&#94;2} \right]} \times
+  *$$
+  **
+  *$$
+  *(\sigma&#94;2)&#94;{-n/2}\left|I + \tilde{X}V\tilde{X}'\right|&#94;{-n/2}\exp{\left[-\frac{1}{2\sigma&#94;2}(\tilde{X} - \tilde{X}\hat{\beta})'\left[I + \tilde{X}V\tilde{X}' \right]&#94;{-1}(\tilde{X} - \tilde{X}\hat{\beta}) \right]} d\sigma
+  * $$
+  **
+  * $$
+*\propto \int \sigma&#94;{-(n - k + 1)/2}\left|I + \tilde{X}V\tilde{X}'\right|&#94;{-n/2} \exp{\left[ -\frac{1}{2\sigma&#94;2}\left( \nu s&#94;2  + (\tilde{X} - \tilde{X}\hat{\beta})'\left[I + \tilde{X}V\tilde{X}' \right]&#94;{-1}(\tilde{X} - \tilde{X}\hat{\beta}) \right) \right]} d\sigma
+* $$
+* $$
+*\propto \left|I + \tilde{X}V\tilde{X}'\right|&#94;{-n/2} \int \sigma&#94;{-(\nu + 1)/2} \exp{\left[-\frac{1}{2\sigma&#94;2}A \right]} d\sigma&#94;2
+* $$
+*Where $A = \left( \nu s&#94;2  + (\tilde{X} - \tilde{X}\hat{\beta})'\left[I + \tilde{X}V\tilde{X}' \right]&#94;{-1}(\tilde{X} - \tilde{X}\hat{\beta}) \right)$ and we substitute $z = \frac{A}{2\sigma&#94;2}$ as described in "Quadratic form statistics" on wikipedia (see below) to produce
+* $$
+*p(\tilde{Y}|Y,\tilde{X},X) \propto A&#94;{-\frac{\nu + 1}{2}}\int z&#94;{(\nu - 1)/2}\exp(-z)dz
+* $$
+*which evaluates to
+* $$
+*\left[ 1 + \frac{(\tilde{X} - \tilde{X}\hat{\beta})'\left[I + \tilde{X}V\tilde{X}' \right]&#94;{-1}(\tilde{X} - \tilde{X}\hat{\beta})}{\nu s&#94;2} \right]&#94;{-\frac{1}{2}(\nu + 1)}
+* $$
+*which is a multivariate t-distribution centered at $\tilde{X}\hat{\beta}$ and scaled by $[I+\tilde{X}V\tilde{X}'] s&#94;2$ with $\nu = n - k$ degrees of freedom.
+  *
+ * The posterior predictive density can be used to draw samples in performing simulation of the distribution of $\tilde{Y}$.
   *
   * <h3>Credible Intervals and Highest Posterior Density</h3>
   *
@@ -223,103 +228,103 @@ which is a multivariate t-distribution centered at $\tilde{X}\hat{\beta}$ and sc
   * for visual comparison of parameter estimates. The parameter space may be partitioned such that
   *
   * $$
-\beta = \begin{bmatrix}
-	\beta_1 \\
-	\beta_2
-\end{bmatrix} \begin{matrix}
- r \\
- k - r
- \end{matrix}
-$$
-$$
-\hat{\beta} = \begin{bmatrix}
- \hat{\beta}_1 \\
- \hat{\beta}_2
- \end{bmatrix} \begin{matrix}
- r \\
- k - r
- \end{matrix}
-$$
-
-$$
-X'X = \begin{bmatrix}
-  ~ & r & k - r \\
-   r & X_1'X_1 & X_1'X_2 \\
-  k - r & X_2'X_1 & X_2'X_2
-  \end{bmatrix}
-  $$
-
-$$
-(X'X)&#94;{-1} = C = \begin{bmatrix}
-~ & r & k - r \\
-r & C_{11} & C_{12} \\
-k - r & C_{21} & C_{22}
-  \end{bmatrix}
-$$
-
-where $r < k$. The subset $\theta_i$ has the property of being normally distributed
-$$
-p(\theta_i|\sigma&#94;2,Y,X) \sim N\left(\hat{\theta_i}, \sigma&#94;2c_{ii}\right)
-$$
-where $c_ii$ is the $ith$ diagonal element of $C$, and the conditional distribution of $p(\theta_2|\theta_1,\sigma&#94;2,Y,X)$ is normally distributed such that
-$$
-p(\theta_2|\theta_1,\sigma&#94;2,Y,X) \sim N\left(\hat{\theta_{2,1}}, \sigma&#94;2(X_2'X)&#94;{-1}\right)
-$$
-where $\hat{\theta_{2,1}} = \hat{\theta_2} + (X_2'X_2)&#94;{-1}X_2X_1(\theta_1 - \hat{\theta_1})$.
-Once partitioned the regions of the highest posterior density can be computed for the parameters $\theta_1$ and $\theta_2$ and charted for comparison.
-
-  *  <h3>Update Rules</h3>
+*\beta = \begin{bmatrix}
+  *\beta_1 \\
+  *\beta_2
+*\end{bmatrix} \begin{matrix}
+  *r \\
+  *k - r
+  *\end{matrix}
+* $$
+* $$
+*\hat{\beta} = \begin{bmatrix}
+  *\hat{\beta}_1 \\
+  *\hat{\beta}_2
+  *\end{bmatrix} \begin{matrix}
+  *r \\
+  *k - r
+  *\end{matrix}
+  *$$
+  **
+  * $$
+*X'X = \begin{bmatrix}
+  *~ & r & k - r \\
+  *r & X_1'X_1 & X_1'X_2 \\
+  *k - r & X_2'X_1 & X_2'X_2
+  *\end{bmatrix}
+  * $$
+  **
+  *$$
+*(X'X)&#94;{-1} = C = \begin{bmatrix}
+*~ & r & k - r \\
+*r & C_{11} & C_{12} \\
+*k - r & C_{21} & C_{22}
+  *\end{bmatrix}
+  * $$
+  **
+ *where $r < k$. The subset $\theta_i$ has the property of being normally distributed
+  *$$
+*p(\theta_i|\sigma&#94;2,Y,X) \sim N\left(\hat{\theta_i}, \sigma&#94;2c_{ii}\right)
+  *$$
+*where $c_ii$ is the $ith$ diagonal element of $C$, and the conditional distribution of $p(\theta_2|\theta_1,\sigma&#94;2,Y,X)$ is normally distributed such that
+  *$$
+*p(\theta_2|\theta_1,\sigma&#94;2,Y,X) \sim N\left(\hat{\theta_{2,1}}, \sigma&#94;2(X_2'X)&#94;{-1}\right)
+  *$$
+*where $\hat{\theta_{2,1}} = \hat{\theta_2} + (X_2'X_2)&#94;{-1}X_2X_1(\theta_1 - \hat{\theta_1})$.
+*Once partitioned the regions of the highest posterior density can be computed for the parameters $\theta_1$ and $\theta_2$ and charted for comparison.
+ *
+ *  <h3>Update Rules</h3>
   *
   *
-The bayesian method allows the model to be adjusted in light of new observations by using the posterior distribution as the prior distribution. This allows the parameters to be adjusted given new evidence. The posterior distribution for the parameters factors into
-$$
-p(\theta,\sigma&#94;2|Y,X) \propto p(\theta|\sigma&#94;2,Y,X) \times p(\sigma&#94;2|Y,X)
-$$
-$$
-p(\theta,\sigma&#94;2|Y,X) \propto N\left(\hat{\beta},\sigma&#94;2V_\beta\right) \times Inverse-\chi&#94;2(\nu,s&#94;2)
-$$
-For the uniform variance it is possible to update first the distribution of $\sigma&#94;2$ and then the distribution of $\beta|\sigma&#94;2$.
-Since in the posterior distribution of $\sigma&#94;2$ we have two parameters the degrees of freedom $\nu = n - k$ and
-the scale $s&#94;2 = \frac{1}{\nu}(Y - X\hat{\beta})'(Y-X\hat{\beta})$.
-When faced with a new set of evidence and observation variables $X_{new}$  with rank $k$ and $Y_{new}$ having variance $\sigma&#94;2$
-we consider the distribution of the variance in the new data to also be the $Inverse-\chi&#94;2$ distribution with $n_{new} - k$ degrees of freedom
-and scale $s'&#94;2 = \frac{1}{n_{new} - k}(Y_{new} - X_{new}\hat{\beta})'(Y_{new} - X_{new}\hat{\beta})$.
-As this is the conjugate prior the update rules (from Boltstad) are as follows.
-$$
-s_{n}&#94;2 = s&#94;2 + \frac{1}{n_{new} - k}(Y_{new} - X_{new}\hat{\beta})'(Y_{new} - X_{new}\hat{\beta})
-$$
-and the degree of freedom
-$$
-\nu_{n} = \nu + (n_{new} - k)
-$$
-This gives the updated distribution for $\sigma&#94;2$ as $Inverse-\chi&#94;2(\nu_{n}, s_{n}&#94;2)$.
-The normal distribution for $p(\theta|\sigma&#94;2,Y,X)$ has the initial parameters
-$$
-\mu_0 = \hat{\beta} = V_\beta X'y
-$$
-$$
-\Lambda_0 = V_\beta
-$$
-where $V_\beta = (X'X)&#94;{-1}$.
-When performing the update with the new data we consider the new data to be normally distributed with uniform variance such that $p(\mu|\Sigma,Y_{new},X_{new}) \sim N\left(\mu,\Sigma/n_{new}\right)$.
-The normal is the conjugate prior so that the update rules can be applied as follows
-$$
-\Lambda_n = \Lambda_0&#94;{-1} + \Sigma&#94;{-1}
-$$
-$$
-\mu_n = (\Lambda_0&#94;{-1} + \Sigma&#94;{-1})&#94;{-1}(\Lambda_0&#94;{-1}\mu_0 + \Sigma&#94;{-1}\bar{Y}_{new})
-$$
-where $\Sigma = (X_{new}'X_{new})$. The updated distribution for $\beta$ is
-$$
-p(\beta|\sigma&#94;2,Y_{new},Y,X_{new},X) \sim N\left(\mu_n, \sigma&#94;2\Lambda_n\right)
-$$
-The updated joint distribution becomes
-$$
-p(\beta,\sigma&#94;2|Y_{new},Y,X_{new},X) \propto  N\left(\mu_n, \sigma&#94;2\Lambda_n\right) \times Inverse-\chi&#94;2(\nu_n, s_n&#94;2)
-$$
-
-
+*The bayesian method allows the model to be adjusted in light of new observations by using the posterior distribution as the prior distribution. This allows the parameters to be adjusted given new evidence. The posterior distribution for the parameters factors into
+  *$$
+*p(\theta,\sigma&#94;2|Y,X) \propto p(\theta|\sigma&#94;2,Y,X) \times p(\sigma&#94;2|Y,X)
+  *$$
+  *$$
+*p(\theta,\sigma&#94;2|Y,X) \propto N\left(\hat{\beta},\sigma&#94;2V_\beta\right) \times Inverse-\chi&#94;2(\nu,s&#94;2)
+  *$$
+*For the uniform variance it is possible to update first the distribution of $\sigma&#94;2$ and then the distribution of $\beta|\sigma&#94;2$.
+*Since in the posterior distribution of $\sigma&#94;2$ we have two parameters the degrees of freedom $\nu = n - k$ and
+*the scale $s&#94;2 = \frac{1}{\nu}(Y - X\hat{\beta})'(Y-X\hat{\beta})$.
+*When faced with a new set of evidence and observation variables $X_{new}$  with rank $k$ and $Y_{new}$ having variance $\sigma&#94;2$
+*we consider the distribution of the variance in the new data to also be the $Inverse-\chi&#94;2$ distribution with $n_{new} - k$ degrees of freedom
+*and scale $s'&#94;2 = \frac{1}{n_{new} - k}(Y_{new} - X_{new}\hat{\beta})'(Y_{new} - X_{new}\hat{\beta})$.
+*As this is the conjugate prior the update rules (from Boltstad) are as follows.
+  *$$
+*s_{n}&#94;2 = s&#94;2 + \frac{1}{n_{new} - k}(Y_{new} - X_{new}\hat{\beta})'(Y_{new} - X_{new}\hat{\beta})
+  *$$
+*and the degree of freedom
+  *$$
+*\nu_{n} = \nu + (n_{new} - k)
+  *$$
+*This gives the updated distribution for $\sigma&#94;2$ as $Inverse-\chi&#94;2(\nu_{n}, s_{n}&#94;2)$.
+*The normal distribution for $p(\theta|\sigma&#94;2,Y,X)$ has the initial parameters
+  *$$
+*\mu_0 = \hat{\beta} = V_\beta X'y
+  *$$
+  *$$
+*\Lambda_0 = V_\beta
+  *$$
+*where $V_\beta = (X'X)&#94;{-1}$.
+*When performing the update with the new data we consider the new data to be normally distributed with uniform variance such that $p(\mu|\Sigma,Y_{new},X_{new}) \sim N\left(\mu,\Sigma/n_{new}\right)$.
+*The normal is the conjugate prior so that the update rules can be applied as follows
+  *$$
+*\Lambda_n = \Lambda_0&#94;{-1} + \Sigma&#94;{-1}
+  *$$
+  *$$
+*\mu_n = (\Lambda_0&#94;{-1} + \Sigma&#94;{-1})&#94;{-1}(\Lambda_0&#94;{-1}\mu_0 + \Sigma&#94;{-1}\bar{Y}_{new})
+  *$$
+*where $\Sigma = (X_{new}'X_{new})$. The updated distribution for $\beta$ is
+  *$$
+*p(\beta|\sigma&#94;2,Y_{new},Y,X_{new},X) \sim N\left(\mu_n, \sigma&#94;2\Lambda_n\right)
+  *$$
+*The updated joint distribution becomes
+  *$$
+*p(\beta,\sigma&#94;2|Y_{new},Y,X_{new},X) \propto  N\left(\mu_n, \sigma&#94;2\Lambda_n\right) \times Inverse-\chi&#94;2(\nu_n, s_n&#94;2)
+  *$$
   *
+ *
+ *
   *
   * <h3>References</h3>
   *
@@ -332,12 +337,155 @@ $$
   * Bishop Christopher M. <i>Pattern Recognition and Machine Learning</i>. Springer Science + Business Media LLC 2006
   *
   * Bolstad William M, <i>Introduction to Bayesian Statistics</i>. Wiley 2nd edition, 2007.
-
-  * "Quadratic form (statistics)", <i>Wikipedia</i> 29th July 2015, 5 Nov 2015. <a href="https://en.wikipedia.org/wiki/Quadratic_form_(statistics)">https://en.wikipedia.org/wiki/Quadratic_form_(statistics)</a>
+ *
+ * "Quadratic form (statistics)", <i>Wikipedia</i> 29th July 2015, 5 Nov 2015. <a href="https://en.wikipedia.org/wiki/Quadratic_form_(statistics)">https://en.wikipedia.org/wiki/Quadratic_form_(statistics)</a>
   *
+  *
+  * <h3>Implementation and Usage</h3>
+  *
+  * This class implements the bayes linear regression for uniform variance.
+  * It extends the OLS method and adds an Update method in order to update the generated model in light of new data
+  * using the update rules above.
+  *
+  *
+  * This class
   *
   * Created by cd on 17/04/2016.
   */
-class BayesLinearRegression {
+class BayesLinearRegression(@transient var inX: DenseMatrix[Double], @transient var inY: DenseVector[Double], override val m: Int = 1)
+  extends OrdLeastSquares(inX, inY, m) {
+
+
+  /**
+    * implement the update rules for the linear regression model after already having trained the model.
+    *
+    * This will update the parameter matrix Beta and the variance.
+    *
+    * it will not predict against the data, after updating use the predict methods to get a prediction and calculate residuals.
+    *
+    * @param newX
+    * @param newY
+    */
+  def update(newX:DenseMatrix[Double], newY:DenseVector[Double]) = {
+    val newP = createPolynomial(newX, m)
+
+    val newY1 = DenseMatrix.tabulate(newY.length, 1) { case (i, j) => newY(i) }
+
+    val prevB = Beta
+    val prevSigma = variance
+
+    val lastdf = P.rows - P.cols
+    val nu = lastdf + (newP.rows - newP.cols)
+
+    val prevMu = 1.0/Y.length * Y.foldLeft (0.0) { (accum, y) => accum + y }
+
+    val mu = 1.0 / newY.length * newY.foldLeft(0.0) { (accum, y) => accum + y }
+
+    val prevSd = 1.0/(Y.length - 1) * Y.foldLeft(0.0) { (accum, y) => accum + Math.pow((y - prevMu), 2) }
+
+    val sd = 1.0/(newY.length - 1) * newY.foldLeft(0.0) { (accum, y) => accum + Math.pow((y - mu), 2) }
+
+    val muC = (prevMu + mu)/2.0
+
+    // the new uniform variance
+    val sigma = (P.rows * (prevSd + Math.pow(prevMu - muC, 2)) + newP.rows * (sd + Math.pow(mu - muC, 2)) ) / (P.rows + newP.rows)
+    val newVariance = Math.pow(sigma, 2)
+
+    /**
+      * determine the new value for $\sigmaI$ from the prior work I've done in my research assignments
+      * the new $I$ is not the identity matrix, but it taken from the diagonal of the $V_\beta = (X'X)^{-1}$ parameter.
+      * The diagonal is used in the OLS as the $v_j$ parameter for the variance of the $\beta$ parameter.
+      **/
+    val newI = DenseMatrix.tabulate(pseudoInverse.rows, pseudoInverse.cols) {
+      case (i,j) => i == j match {
+        case true => pseudoInverse(j, j)
+        case _ => 0.0
+      }
+    }
+    val mu0 = Beta
+    val lambda0 = pseudoInverse
+    val cov = newVariance * newI
+    val lambdaNew = lambda0 + cov
+
+    /**
+      * from R
+      *
+      * ones of mu2
+      *
+      * MeanVec <- as.matrix(sample(x = c(1), size = nrow(Mu0), replace = TRUE) * mu2)
+
+
+
+    Mu2 <- ginv(ginv(Lambda0) + n2*ginv(Lambda2)) %*%(ginv(Lambda0)%*%Mu0 + n2*ginv(Cov)%*%MeanVec)
+   Mu2 <- data.frame(Mu2)
+   rownames(Mu2) <- rownames(Mu0)
+   result2$V <- Lambda2
+   result2$B <- as.matrix(Mu2)
+      */
+
+    /**
+      * calculate the new Beta
+      *
+      *   Mu2 <- ginv( ginv(Lambda0) + n2*ginv(Lambda2) ) %*%(ginv(Lambda0)%*%Mu0 + n2*ginv(Cov)%*%MeanVec)
+          Mu2 <- data.frame(Mu2)
+          result2$B <- as.matrix(Mu2)
+      */
+    // inverse of the inverse
+    val meanVec = DenseMatrix.ones[Double](mu0.rows, 1) * mu
+    val invLambda1 = PseudoInverse(lambda0)
+    val invLambda2 = PseudoInverse(lambdaNew)
+    val invCov = PseudoInverse(cov)
+    val mat1 = invLambda1 + newP.rows * invLambda2
+    val mat2 = invLambda1*mu0 + (newP.rows * invCov)* meanVec
+    val newBeta = PseudoInverse(mat1) * PseudoInverse(mat2)
+    // update the parameters
+    pseudoInverse = lambdaNew
+    Beta = newBeta
+    variance = newVariance
+
+    // estimate $\hat{Y}$
+    val yHat = newY1.t * Beta.t
+    residuals = (Y1 - yHat).toDenseVector
+    mse = squaredError(yHat, Y1)
+
+    // update the z-values and p-values
+    // take the diagonal of the pseudoInverse (X'X)^-1
+    // note that the pseudoinverse actually may be negative, the square root may infact be imaginary.
+    val vj = DenseVector.tabulate(pseudoInverse.cols) {
+      j => pseudoInverse(j, j)
+    }
+    /*
+    * $$
+      * z_j = \frac{\hat{\beta_j}}{\hat{\sigma}\sqrt{v_j}}
+    * $$
+    *
+    * Since we are predicting a 1 column Y vector the beta matrix is always a single column matrix
+    *
+    * The row in beta corresponds to the coefficient of the ith column in X.
+    *
+    */
+    betaZScore = DenseMatrix.tabulate[Double](Beta.rows, Beta.cols) {
+      case (i, j) => Beta(i, j) / sigma * Math.sqrt(vj(i))
+    }
+
+    /**
+      * the corresponding p-value for beta_j can be calculated
+      * from the T distribution with N - p - 1 degrees of freedom.
+      * The null hypothesis is that beta_j is equal to 0, and therefore the corresponding X value does not contribute
+      * to the prediction.
+      *
+      * Testing against the hypothesis we reject the hypothesis if { |Z| > 0 }
+      */
+    val stdNorm = Normal(0.0)(1.0)
+    betaPValue = DenseMatrix.tabulate[Double](betaZScore.rows, betaZScore.cols) {
+      case (i, j) => stdNorm.pdf(betaZScore(i, j))
+    }
+
+    val criticalValue = CriticalValue.upperCriticalValue(0.0, 4 * 1.0, stdNorm)
+    criticalBetaZValue = criticalValue.value(alpha)
+    criticalPValue = stdNorm.pdf(criticalBetaZValue)
+
+    (yHat, mse)
+  }
 
 }
