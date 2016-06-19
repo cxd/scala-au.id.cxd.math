c65417041c26763e80ea2e65048463c178c3d48a
Chris Davey <cd@cpd.id.au>
2016-06-19 12:24:16 +1000

    updated documents

--- update_docs.sh
+++ update_docs.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+echo Create scaladoc
+cd /Users/cd/Projects/scala/au.id.cxd.math/
+sbt doc
+
+echo Copy to site
+cd /Users/cd/Projects/scala/scala-au.id.cxd.math-gh-pages/scala-au.id.cxd.math/
+pwd
 rsync --delete -azzv /Users/cd/Projects/scala/au.id.cxd.math/math/target/scala-2.11/api ./latest/math/
 git add .
 git commit -m "updated documents" -a
