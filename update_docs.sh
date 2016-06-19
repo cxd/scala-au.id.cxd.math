7920bdfc45bcbc1ff82311ab585c6313e5c3d503
Chris Davey <cd@cpd.id.au>
2016-06-19 12:19:37 +1000

    updated documents

--- update_docs.sh
+++ update_docs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-rsync --delete -azzv /Users/cd/Projects/scala/au.id.cxd.math/math/target/scala-2.11/api ./latest/math
+rsync --delete -azzv /Users/cd/Projects/scala/au.id.cxd.math/math/target/scala-2.11/api ./latest/math/
 git add .
 git commit -m "updated documents" -a
 git push origin gh-pages
