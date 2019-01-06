c1428b23c87483aed2040079515ebb9256512fd2
Chris Davey <cpdavey@gmail.com>
2019-01-06 17:03:51 +1000

    initial skeleton for gibbs sampling driver.

--- math/src/main/scala/au/id/cxd/math/model/particle/mcmc/GibbsSampling.scala
+++ math/src/main/scala/au/id/cxd/math/model/particle/mcmc/GibbsSampling.scala
@@ -5,23 +5,24 @@ import au.id.cxd.math.probability.random.RNormal
 
 /**
   * initialise with
+  *
   * @param initialState
-  *                     The initial state representation.
+  * The initial state representation.
   * @param proposal
-  *                 The proposal generator for new states.
+  * The proposal generator for new states.
   * @param logLikelihoodFn
-  *                        The log likelihood function p(s|X)
-  *                        the likelihood of the state given the data.
+  * The log likelihood function p(s|X)
+  * the likelihood of the state given the data.
   */
-class GibbsSampling(override val initialState: State, override val proposal: Proposal, likelihood:StateLikelihood) extends McmcSampling {
+class GibbsSampling(override val initialState: State, override val proposal: Proposal, likelihood: StateLikelihood) extends McmcSampling {
 
   /**
     * mutable values storing counts of accepted states over the iteration.
     * it could be possible to trace these over iterations instead.
     * maybe change these to a mutable collection over time if desired.
     */
-  var accepted:Double = 0.0
-  var total:Double = 0.0
+  var accepted: Double = 0.0
+  var total: Double = 0.0
 
   /**
     * random sample for acceptance test.
@@ -30,16 +31,17 @@ class GibbsSampling(override val initialState: State, override val proposal: Pro
 
   /**
     * Take a single step and determine which state to retain as the next state.
+    *
     * @param i
     * @param prev
     * @return
     */
   override def step(i: Int, prev: State): State = {
-    val (nextState, q1,q2) = proposal.propose(i, prev, likelihood)
-    val qratio = q1/q2
+    val (nextState, q1, q2) = proposal.propose(i, prev, likelihood)
+    val qratio = q1 / q2
     val prevLogP = likelihood.logLikelihood(prev)
     val nextLogP = likelihood.logLikelihood(nextState)
-    val scores = Array(1.0, qratio*Math.exp(nextLogP - prevLogP))
+    val scores = Array(1.0, qratio * Math.exp(nextLogP - prevLogP))
     val alpha = scores.min
     val r = rnorm.draw()
     // acceptance test
@@ -54,4 +56,10 @@ class GibbsSampling(override val initialState: State, override val proposal: Pro
     }
   }
 
+}
+
+object GibbsSampling {
+
+  def apply(initialState: State, proposal: Proposal, likelihood: StateLikelihood) =
+    new GibbsSampling(initialState, proposal, likelihood)
 }
