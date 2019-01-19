8ea14a98aba9c50e6ce85ed583db25bb618c9fe2
Chris Davey <cpdavey@gmail.com>
2019-01-20 09:39:29 +1000

    update RBinom in the case of small means

--- math/src/main/scala/au/id/cxd/math/probability/random/RBinom.scala
+++ math/src/main/scala/au/id/cxd/math/probability/random/RBinom.scala
@@ -192,11 +192,11 @@ class RBinom(val n: Double, val p: Double) extends RandomDeviate {
   override def draw(): Double = {
     // the probability is swapped when p > 0.5
     if (p > 0.5) {
-      val ix = if (np < SMALL_MEAN) smallMeanEstimate(1.0 - p)
+      val ix = if (np < SMALL_MEAN) smallMeanEstimate(p)
       else triangularEstimate(1.0 - p, p, n * (1.0 - p))
       n - ix
     } else {
-      val ix = if (np < SMALL_MEAN) smallMeanEstimate(p)
+      val ix = if (np < SMALL_MEAN) smallMeanEstimate(1.0 - p)
       else triangularEstimate(p, q, n * p)
       ix
     }
--- math/src/test/scala/probability/random/TestRBinom.scala
+++ math/src/test/scala/probability/random/TestRBinom.scala
@@ -5,16 +5,22 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class TestRBinom extends FlatSpec with Matchers {
 
-  // TODO: work on debugging the RBinom distribution
   "rbinom" should "Draw random samples from distribution" in {
     val binom = RBinom(n=5, p=0.3)
-    val samples = binom.draw(100)
+    val samples = binom.draw(1000)
     println(samples)
     // note that larger samples of binomial should have a distribution that is approximately normal
   }
 
   "rapproxbinom" should "Draw random samples from distribution" in {
     val binom = RApproxBinom(n=5, p=0.3)
+    val samples = binom.draw(100)
+    println(samples)
+    // note that larger samples of binomial should have a distribution that is approximately normal
+  }
+
+  "rbinom" should "Draw random samples from distribution with large n" in {
+    val binom = RBinom(n=25, p=0.3)
     val samples = binom.draw(100)
     println(samples)
     // note that larger samples of binomial should have a distribution that is approximately normal
