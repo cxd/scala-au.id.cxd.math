b9f8fa630288ef767a21a5798df976208cb53833
Chris Davey <cpdavey@gmail.com>
2019-03-02 17:39:47 +1000

    added some model evaluation measures, R^2, MAE, MSE, WillmotsIndex and coefficient of efficiency

--- math/src/main/scala/au/id/cxd/math/model/evaluation/WillmotsIndex.scala
+++ math/src/main/scala/au/id/cxd/math/model/evaluation/WillmotsIndex.scala
@@ -3,12 +3,23 @@ package au.id.cxd.math.model.evaluation
 import au.id.cxd.math.function.moments.Mean
 
 /**
+  * Index of Agreement.
+  *
   * Calculate wilmots index for the power j.
   * Defaults to j=2.
   *
   *
+  *
   * d_j = 1.0 - \frac{\sum_{i=1}|O_i - P_i|^j}{\sum_{i=1}(|P_i-\bar{O}| + |O_i - \bar{O}|)^j}
  *
+  * Note that using the exponent j=1 the variation is given their true weighting.
+  * d_1 avoids the inflation that the squared error is vulnerable to in d_2
+  *
+  * d_1 is the index of agreement for j=1
+  * d_2 is the index of agreement for j=2
+  *
+  * See Legates and Mccabe "Evaluating "Goodness of fit" measures 1999.
+  *
   * @param j
   * @param obs
   * @param pred
